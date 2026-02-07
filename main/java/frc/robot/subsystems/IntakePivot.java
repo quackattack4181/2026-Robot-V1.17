584d144bf8fbf4eb7b1d8abc9c60895907bec0a3
QuackAttack-Team4181 <162037610+quackattack4181@users.noreply.github.com>
2026-02-06 18:50:51 -0600

    Log intake pivot angle

--- main/java/frc/robot/subsystems/IntakePivot.java
+++ main/java/frc/robot/subsystems/IntakePivot.java
@@ -6,15 +6,21 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakePivot extends SubsystemBase implements AutoCloseable {
   private final SparkMax pivotMotor;
+  private final DutyCycleEncoder pivotEncoder;
+  private double lastPrintTime;
 
   public IntakePivot() {
     pivotMotor = new SparkMax(IntakeConstants.PIVOT_MOTOR_ID, MotorType.kBrushless);
+    pivotEncoder = new DutyCycleEncoder(1);
+    lastPrintTime = 0.0;
 
     SparkMaxConfig pivotConfig = new SparkMaxConfig();
     pivotConfig.idleMode(IdleMode.kBrake);
@@ -36,7 +42,18 @@ public class IntakePivot extends SubsystemBase implements AutoCloseable {
   }
 
   @Override
+  public void periodic() {
+    double now = Timer.getFPGATimestamp();
+    if (now - lastPrintTime >= 0.25) {
+      double degrees = pivotEncoder.getAbsolutePosition() * 360.0;
+      System.out.printf("Intake pivot angle: %.2f degrees%n", degrees);
+      lastPrintTime = now;
+    }
+  }
+
+  @Override
   public void close() {
     pivotMotor.close();
+    pivotEncoder.close();
   }
 }
