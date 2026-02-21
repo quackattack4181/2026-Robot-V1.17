a2fb89917bdcc51e4db8e7d755d724b70195d36d
QuackAttack-Team4181 <162037610+quackattack4181@users.noreply.github.com>
2026-02-21 09:16:12 -0600

    Enable two controller mode by default

--- main/java/frc/robot/Constants.java
+++ main/java/frc/robot/Constants.java
@@ -50,7 +50,7 @@ public final class Constants
     public static final double LEFT_Y_DEADBAND  = 0.1;
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT    = 6;
-    public static final boolean TWO_CONTROLLER_MODE = false;
+    public static final boolean TWO_CONTROLLER_MODE = true;
     public static final boolean CLIMBER_ENABLED = false;
   }
   public static class VisionConstants
